b23e94f8fa7432e14e81eb92083af2e996039fab
chenxu565 <106840474+chenxu565@users.noreply.github.com>
2022-06-11 17:12:58 +0000

    task1

--- src/index.js
+++ src/index.js
@@ -10,4 +10,25 @@ if (document.readyState !== "loading") {
 
 function initializeCode() {
   document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
+  for (let i = 0; i < 5; i++) {
+    let body = document.body;
+    let wikiHeader = document.createElement("h1");
+    wikiHeader.classList.add("wiki-header");
+    wikiHeader.innerHTML = "Bread X";
+    body.appendChild(wikiHeader);
+    let wikiContent = document.createElement("div");
+    wikiContent.classList.add("wiki-content");
+    let paragraph = document.createElement("p");
+    paragraph.classList.add("wiki-text");
+    paragraph.innerHTML = "Some text about this bread.";
+    let imageContainer = document.createElement("div");
+    imageContainer.classList.add("img-container");
+    let wikiImg = document.createElement("img");
+    wikiImg.classList.add("wiki-img");
+    wikiImg.src = "";
+    imageContainer.appendChild(wikiImg);
+    wikiContent.appendChild(paragraph);
+    wikiContent.appendChild(imageContainer);
+    body.appendChild(wikiContent);
+  }
 }
