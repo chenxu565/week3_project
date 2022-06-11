0616738d8de7916d9472394d80aea979e7ca862e
chenxu565 <106840474+chenxu565@users.noreply.github.com>
2022-06-11 20:31:43 +0000

    Add lorem ipsum

--- src/index.js
+++ src/index.js
@@ -41,7 +41,8 @@ function initItem(container, data) {
 
   let paragraph = document.createElement("p");
   paragraph.classList.add("wiki-text");
-  paragraph.innerHTML = "Some text about this bread.";
+  paragraph.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   let imageContainer = document.createElement("div");
   imageContainer.classList.add("img-container");
