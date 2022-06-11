f49c02dcfc3339c0aafdd1ed1199930dc873e5bd
chenxu565 <106840474+chenxu565@users.noreply.github.com>
2022-06-11 18:08:06 +0000

    Create an initItem() function

--- src/index.js
+++ src/index.js
@@ -15,37 +15,50 @@ function initializeCode() {
   container.classList.add("container");
 
   for (let i = 0; i < 5; i++) {
-    let wikiItem = document.createElement("div");
-    wikiItem.classList.add("wiki-item");
-
-    let wikiHeader = document.createElement("h1");
-    wikiHeader.classList.add("wiki-header");
-    wikiHeader.innerHTML = "Breed X";
-    wikiItem.appendChild(wikiHeader);
-
-    let wikiContent = document.createElement("div");
-    wikiContent.classList.add("wiki-content");
-
-    let paragraph = document.createElement("p");
-    paragraph.classList.add("wiki-text");
-    paragraph.innerHTML = "Some text about this bread.";
-
-    let imageContainer = document.createElement("div");
-    imageContainer.classList.add("img-container");
-
-    let wikiImg = document.createElement("img");
-    wikiImg.classList.add("wiki-img");
-    wikiImg.src = "";
-
-    imageContainer.appendChild(wikiImg);
-
-    wikiContent.appendChild(paragraph);
-    wikiContent.appendChild(imageContainer);
-
-    wikiItem.appendChild(wikiContent);
-
+    let wikiItem = initItem();
     container.appendChild(wikiItem);
-
-    body.appendChild(container);
   }
+
+  body.appendChild(container);
+
+  /*
+  fetch("https://dog.ceo/api/breeds/image/random")
+  .then(response=> response.json())
+  .then(data=>{
+    console.log(data);
+  });
+    */
+}
+
+function initItem() {
+  let wikiItem = document.createElement("div");
+  wikiItem.classList.add("wiki-item");
+
+  let wikiHeader = document.createElement("h1");
+  wikiHeader.classList.add("wiki-header");
+  wikiHeader.innerHTML = "Breed X";
+  wikiItem.appendChild(wikiHeader);
+
+  let wikiContent = document.createElement("div");
+  wikiContent.classList.add("wiki-content");
+
+  let paragraph = document.createElement("p");
+  paragraph.classList.add("wiki-text");
+  paragraph.innerHTML = "Some text about this bread.";
+
+  let imageContainer = document.createElement("div");
+  imageContainer.classList.add("img-container");
+
+  let wikiImg = document.createElement("img");
+  wikiImg.classList.add("wiki-img");
+  wikiImg.src = "";
+
+  imageContainer.appendChild(wikiImg);
+
+  wikiContent.appendChild(paragraph);
+  wikiContent.appendChild(imageContainer);
+
+  wikiItem.appendChild(wikiContent);
+
+  return wikiItem;
 }
