30a2b961944ea1b5f8151356227e6171e308b7b0
chenxu565 <106840474+chenxu565@users.noreply.github.com>
2022-06-13 06:05:59 +0000

    Task5

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 //import "./styles.css";
 
 let breeds = ["African", "Pekinese", "Cockapoo", "Beagle", "Briard"];
-/*
-let fetch_addr = [
-  "https://dog.ceo/api/breed/african/images/random",
-  "https://dog.ceo/api/breed/pekinese/images/random",
-  "https://dog.ceo/api/breed/cockapoo/images/random",
-  "https://dog.ceo/api/breed/beagle/images/random",
-  "https://dog.ceo/api/breed/briard/images/random"
+
+let wiki_addr = [
+  "https://en.wikipedia.org/api/rest_v1/page/summary/Africanis",
+  "https://en.wikipedia.org/api/rest_v1/page/summary/Pekingese",
+  "https://en.wikipedia.org/api/rest_v1/page/summary/Cockapoo",
+  "https://en.wikipedia.org/api/rest_v1/page/summary/Beagle",
+  "https://en.wikipedia.org/api/rest_v1/page/summary/Briard"
 ];
-*/
+/*
 let description = [
   "Considered a landrace with limited human interference in their breeding, the Africanis has also been maintained by human owners. The Africanis is a medium-sized, lightly built dog with a long slender muzzle and, usually, a short coat. It has been described as resembling a cross between a Greyhound and a Dingo.",
   "The Pekingese is a breed of toy dog, originating in China. The breed was favored by royalty of the Chinese Imperial court as both a lap dog and companion dog, and its name refers to the city of Peking where the Forbidden City is located. ",
   "A cockapoo also known as a spoodle or cockerdoodle is a dog crossbreed bred from a Cocker Spaniel and a Poodle, most commonly the Miniature Poodle.",
   "The beagle breed is a small scent hound, similar in appearance to the much larger foxhound. The beagle was developed primarily for hunting hare known as beagling. Possessing a great sense of smell and superior tracking instincts, the beagle is the primary breed used as a detection dog for prohibited agricultural imports and foodstuffs in quarantine around the world. The beagle is intelligent. It is a popular pet due to its size, good temper, and a lack of inherited health problems. ",
   "The Briard or Berger de Brie is a French breed of large shepherd dog, traditionally used both for herding sheep and to defend them. It was first shown at the first Paris dog show, in 1863; the first Briard to be registered in the Livre des Origines Françaises, the national stud-book, was Sans Gêne in 1885."
-];
+];*/
 
 if (document.readyState !== "loading") {
   initializeCode();
@@ -77,8 +77,14 @@ function initItem_(container, index) {
       //let bn = data["message"].split("/")[4];
       let src = data["message"];
       //wikiHeader.innerHTML = bn;
-      paragraph.innerHTML = description[index];
+      //paragraph.innerHTML = description[index];
       wikiImg.src = src;
+    });
+
+  fetch(wiki_addr[index])
+    .then((response) => response.json())
+    .then((data) => {
+      paragraph.innerHTML = data["extract"];
     });
 
   imageContainer.appendChild(wikiImg);
